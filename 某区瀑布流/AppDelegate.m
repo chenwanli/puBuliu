1201d1097b44b1c3c73ea78262b68269e216796b
wanli <137199179@qq.com>
2017-01-03 10:03:26 +0800

    d
    d

--- 某区瀑布流/AppDelegate.m
+++ 某区瀑布流/AppDelegate.m
@@ -16,7 +16,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    //1、添加第一行
+    
     return YES;
 }
 
